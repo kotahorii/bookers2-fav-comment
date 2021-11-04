863fb1c068eb8f16113e54852f5c680dcd1aeeaa
kotahorii <khorii.micron@gmail.com>
2021-11-04 02:16:10 +0000

    relationship model and controller created

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   attachment :profile_image, destroy: false
 
