48c6b4100da66ac45cb4bf734146845d0f2305b3
kotahorii <khorii.micron@gmail.com>
2021-11-04 07:07:22 +0000

    search bar created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'search' => 'searches#search'
   resources :users,only: [:show,:index,:edit,:update] do
      resources :relationships, only: [:create, :destroy]
      get 'follow' => 'relationships#follow'
